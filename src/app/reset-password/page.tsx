fb503f1558723fee50e5ad5ae66c1675e65d0e34
BlackLex29 <lextermilo@gmail.com>
2025-10-24 09:52:17 +0800

    Normalize line endings with .gitattributes

---
+++ src/app/reset-password/page.tsx
@@ -0,0 +1,11 @@
+// app/reset-password/page.tsx
+import { Suspense } from "react";
+import ResetPasswordContent from "./ResetPasswordContent";
+
+export default function ResetPasswordPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ResetPasswordContent />
+    </Suspense>
+  );
+}
